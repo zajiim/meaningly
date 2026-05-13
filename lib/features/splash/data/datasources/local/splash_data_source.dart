b41d3c10dc186fc1904119bccc755240ec5a1213
Sajim <sajim@Sajims-MacBook-Pro.local>
2026-05-13 14:31:36 +0530

    Added zip file in assets instead of raw json file, unzipped and stored the data in local db

--- lib/features/splash/data/datasources/local/splash_data_source.dart
+++ lib/features/splash/data/datasources/local/splash_data_source.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:isolate';
 
+import 'package:archive/archive.dart';
 import 'package:flutter/services.dart';
 
 abstract class SplashDataSource {
@@ -10,11 +12,24 @@ abstract class SplashDataSource {
 class SplashDataSourceImpl implements SplashDataSource {
   @override
   Future<Map<String, String>> loadDictionaryData() async {
-    final jsonStr = await rootBundle.loadString('assets/data/dictionary_data.json');
+    // final jsonStr = await rootBundle.loadString('assets/data/dictionary_data.json');
+
+    final ByteData data = await rootBundle.load('assets/data/dictionary_data.json.zip');
+    final List<int> bytes = data.buffer.asUint8List();
 
     return await Isolate.run(() {
-      final Map<String, dynamic>decoded = jsonDecode(jsonStr);
-      return decoded.map((key, value) => MapEntry(key, value.toString()));
+      final archive =  ZipDecoder().decodeBytes(bytes);
+      final file = archive.first;
+
+      if(file.isFile) {
+        final String jsonString = utf8.decode(file.content as List<int>);
+        final Map<String, dynamic> decoded = jsonDecode(jsonString);
+        return decoded.map((key, value) => MapEntry(key, value.toString()));
+      }
+
+      throw Exception("Zip file is empty or invalid");
+      // final Map<String, dynamic>decoded = jsonDecode(jsonStr);
+      // return decoded.map((key, value) => MapEntry(key, value.toString()));
     });
   }
 }
